8663978590996310e3bb479664b8809a90ecfb9a
Lilian Zhao <milanzq309@gmail.com>
2021-03-20 04:18:08 -0700

    added chart and functions to control data

--- App.js
+++ App.js
@@ -35,29 +35,32 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    let ctx = document.querySelector('#myChart').getContext('2d')
-    let myChart = new Chart(ctx, {
+    //initializing chart
+    var ctx = document.querySelector('#myChart').getContext('2d')
+    var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+            labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
             datasets: [{
                 label: '# of hours',
                 data: [5, 8, 8, 3, 6, 7, 4],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(95, 138, 255, 0.3)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(95, 138, 255, 0.3)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(95, 138, 255, 0.3)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(95, 138, 255, 0.3)',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(95, 138, 255, 1)', 
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(95, 138, 255, 1)', 
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(95, 138, 255, 1)', 
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(95, 138, 255, 1)', 
                 ],
                 borderWidth: 1
             }]
@@ -71,6 +74,32 @@ document.addEventListener('DOMContentLoaded', () => {
                 }]
             }
         }
+    })
+
+    //add data function
+    function addData(chart) {
+        chart.data.labels.push(document.querySelector('#label').value)
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(document.querySelector('#data').value)
+        })
+        chart.update()
+    }
+    //add data button
+    document.querySelector('#addDataBtn').addEventListener('click', () => {
+        addData(myChart)
+    })
+
+    //remove data function
+    function removeData(chart) {
+        chart.data.labels.pop();
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        chart.update();
+    }
+    //remove data button
+    document.querySelector('#removeDataBtn').addEventListener('click', () => {
+        removeData(myChart)
     })
 
 })
